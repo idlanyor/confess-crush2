455762963f3859bc1cf167b0fbdc3cedb6434314
idlanyor <prasbhara0604@gmail.com>
2025-09-24 19:59:55 +0700

    masaka no confess

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Mau ngga? kalau kita jadian?",
+    introTitle: 'Fatimah',
+    introDesc: `tidak perlu kata kata, 
+    kau mau jadi pacarku ngga
+    `,
+    btnIntro: 'Aduhai',
+    title: 'Jadi gini, mmm... <33',
+    desc: 'mending gak ngoding apa gak turu?',
+    btnYes: 'Gak Turu',
+    btnNo: 'Gak ngoding',
+    question: 'Di dunia ada lebih dari 7 juta orang jawa, mengapa kamu memilih menjadi sunda? <3',
+    btnReply: 'Kirim',
+    reply: 'Karna aku suka kamu',
+    mess: 'Satu oraang jawa, sembilan belas lainnya menunggu lapangan pekerjaan',
+    messDesc: 'Fun Fact,Kota tertua di Indonesia itu palembang.',
+    btnAccept: 'Baiklah <3',
+    messLink: 'https://aduhai.bukankah-ini.my.id'
+}
